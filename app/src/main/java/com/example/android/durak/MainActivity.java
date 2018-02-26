70aea226fa21fe627d466d79ea2e3b11e8132990
samer alameri <samirssaadi@gmail.com>
2018-02-26 07:04:29 -0800

    this is why im hot

--- app/src/main/java/com/example/android/durak/MainActivity.java
+++ app/src/main/java/com/example/android/durak/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.android.durak;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    String name;
+   public static String name;
+   public static int counterVisiting;
+    TextView textView;
+    sql_lite db = new sql_lite(this);
     private int [] imageArray = {R.drawable.logo,R.drawable.n1111111111,R.drawable.nnnnnnn};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +56,40 @@ public class MainActivity extends AppCompatActivity {
 
         name = getIntent().getStringExtra("the name");
 
-        Tname.setText(name);
+
+
         int nu = getIntent().getIntExtra("number",0);
         ImageView profileimage = (ImageView) findViewById(R.id.imaaaage);
         profileimage.setImageResource(imageArray[nu]);
+
+        Tname.setText(name);
+
+        final String PREFS_NAME = "MyPrefsFile";
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+            Log.d("Comments", "First time");
+
+            // first time task
+
+
+            Intent intent = new Intent(this,Option.class);
+            startActivity(intent);
+            finish();
+
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).commit();
+        }
+        else{
+            showdata();
+        }
+
+
+
+
+
 
 
 
@@ -61,6 +100,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void openNewGame(View view) {
+
+        // this for showing the saving data in the SQlite
+        showdata();
+
         Intent intent = new Intent(MainActivity.this,New_Game.class);
         startActivity(intent);
         finish();
@@ -69,9 +112,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void option(View view) {
+
+
         Intent intent = new Intent(this,Option.class);
         startActivity(intent);
         finish();
+
+    }
+
+    public void showdata(){
+        String savedName;
+        savedName = db.showshow();
+        int savedPic = db.showpic();
+
+        ImageView profileimage = (ImageView) findViewById(R.id.imaaaage);
+        profileimage.setImageResource(imageArray[savedPic]);
+
+
+       TextView Tname = (TextView)findViewById(R.id.theName);
+
+     Tname.setText(savedName);
+
     }
 
     public  void  Exit (View view){
--- app/src/main/java/com/example/android/durak/Option.java
+++ app/src/main/java/com/example/android/durak/Option.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Option extends AppCompatActivity {
-String x;
-int theNumberOfTheİmage;
+    sql_lite db = new sql_lite(this);
+        String x;
+        int theNumberOfTheİmage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,13 @@ int theNumberOfTheİmage;
     public void Save(View view) {
         EditText e1 = (EditText) findViewById(R.id.name);
         x = e1.getText().toString();
+        Boolean result2 = db.isertData(x);
+        Boolean result = db.insertPic(theNumberOfTheİmage);
+        if (result==true&&result2==true)
+            Toast.makeText(this,"Saved",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this,"Wrong",Toast.LENGTH_SHORT).show();
+
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.putExtra("the name",x);
         ImageView m1 = (ImageView) findViewById(R.id.profile);
@@ -32,6 +44,8 @@ int theNumberOfTheİmage;
         finish();
 
     }
+
+
 
     public void nextimage(View view) {
         ImageView image1 = (ImageView) findViewById(R.id.profile);
